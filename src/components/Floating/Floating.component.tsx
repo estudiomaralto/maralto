fb22c2043c55f6b7282416a5f07dee366efc3baf
osamuel-cardoso <samuelcardoso555@gmail.com>
2024-04-08 17:19:27 -0300

    Update Floating.component.tsx

--- src/components/Floating/Floating.component.tsx
+++ src/components/Floating/Floating.component.tsx
@@ -8,6 +8,7 @@ if (typeof window !== 'undefined') {
 
 import condeProfile from '@/images/condeProfile.png'
 
+import { Container } from '../Container/Container.component'
 import Availability from './components/Availability.component'
 
 interface FloatingTypes {
@@ -16,28 +17,30 @@ interface FloatingTypes {
 
 export function Floating({ avalibility }: FloatingTypes) {
     return (
-        <div className="fixed bottom-[1vh] left-[50%] z-10 flex -translate-x-1/2 items-center gap-2 rounded-xl bg-slate-950 bg-opacity-75 px-4 py-[.8125rem] backdrop-blur-md md:gap-4">
-            <img
-                src={condeProfile.src}
-                alt="Foto do Marcos Condé"
-                className="h-8 w-8 rounded-full object-cover md:h-12 md:w-12"
-            />
+        <div className="fixed bottom-[1vh] left-[50%] z-10 w-full -translate-x-1/2 px-3 md:max-w-[27.75rem]">
+            <Container className="flex w-full items-center gap-2 rounded-xl bg-slate-950 bg-opacity-75 px-4 py-[.8125rem] backdrop-blur-md md:gap-4">
+                <img
+                    src={condeProfile.src}
+                    alt="Foto do Marcos Condé"
+                    className="h-8 w-8 rounded-full object-cover md:h-12 md:w-12"
+                />
 
-            <div className="flex w-full flex-col gap-[.3125rem]">
-                <span className="text-[.9375rem] font-bold leading-none text-slate-50 md:text-[1.0625rem]">
-                    Entre em contato
-                </span>
-                <div className="flex items-center gap-2">
-                    <span className="text-[.8125rem] font-medium leading-none text-slate-300 md:text-[.8125rem]">
-                        Disponibilidade
+                <div className="flex w-full flex-col gap-[.3125rem]">
+                    <span className="text-[.9375rem] font-bold leading-none text-slate-50 md:text-[1.0625rem]">
+                        Entre em contato
                     </span>
-                    <Availability avalibility={avalibility} />
+                    <div className="flex items-center gap-2">
+                        <span className="text-[.8125rem] font-medium leading-none text-slate-300 md:text-[.8125rem]">
+                            Disponibilidade
+                        </span>
+                        <Availability avalibility={avalibility} />
+                    </div>
                 </div>
-            </div>
 
-            <button className="flex text-nowrap rounded-full bg-pale-50 px-[.9375rem] py-[.6875rem] text-[.9375rem] leading-none transition hover:bg-pale-950 hover:text-pale-50">
-                Vamos conversar 🡥
-            </button>
+                <button className="flex text-nowrap rounded-full bg-pale-50 px-[.9375rem] py-[.6875rem] text-[.8125rem] leading-none transition hover:bg-pale-950 hover:text-pale-50 md:text-[.9375rem]">
+                    Vamos conversar 🡥
+                </button>
+            </Container>
         </div>
     )
 }
