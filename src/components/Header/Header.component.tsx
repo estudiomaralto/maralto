e3701ae411d87b4dfcab796ecf5003a08267c6d1
osamuel-cardoso <samuelcardoso555@gmail.com>
2024-04-18 11:49:47 -0300

    Update Header.component.tsx

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -9,9 +9,9 @@ export async function Header() {
     return (
         <header className="fixed left-0 right-0 top-0 z-20 bg-pale-50 py-5">
             <Container>
-                <div className="flex flex-row-reverse items-center justify-between gap-4 md:flex-row">
+                <div className="flex items-center justify-between gap-4 md:flex-row">
                     <Time className="text-[.875rem] md:text-[1.0625rem]" />
-                    <Link href={'/'} className="self-center">
+                    <Link href={'/'} className="ml-[6vw] md:ml-[3vw]">
                         <Maralto />
                     </Link>
                     <div className="justify-end gap-5 leading-none md:flex md:text-[1.0625rem]">
